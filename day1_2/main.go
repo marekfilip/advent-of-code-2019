12062af1dc3fa190870755627152e4672953ef18
Filip Marek <filip@imarek.eu>
2019-12-08 23:23:11 +0100

    day 1

---
+++ day1_2/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	var mass int
+
+	f, err := os.Open("./input")
+	if err != nil {
+		log.Fatalln("Cant read file")
+	}
+	defer f.Close()
+
+	var requiredFuel int
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		_, err := fmt.Sscanf(s.Text(), "%d", &mass)
+		if err != nil {
+			log.Fatalln("Cant read line from file")
+		}
+
+		requiredFuel += calculateFuel(mass)
+	}
+
+	fmt.Printf("Required fuel: %d\n", requiredFuel)
+}
+
+func calculateFuel(mass int) int {
+	overallFuel := int(mass/3) - 2
+
+	if overallFuel > 0 {
+		return overallFuel + calculateFuel(overallFuel)
+	}
+
+	return 0
+}
